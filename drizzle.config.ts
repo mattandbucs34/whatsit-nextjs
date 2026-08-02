439a5320c07f205d4ac0c8e9b9670101f26abea7
Matt Johnson <mattandbucs@gmail.com>
2026-08-02 18:54:17 -0500

    fix drizzle config path and use drizzle-kit push in CI workflow

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-    schema: './src/db/schemas/*',
+    schema: './src/db/schema.ts',
     out: './src/db/migrations',
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.DATABASE_URL!,
-    }
+    },
 });
